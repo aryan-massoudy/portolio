15cd617f617ee233aaab88d6eb114d9d3b65ec2e
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-05-30 14:04:37 +0200

    prepared for deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rf*otv#+b%vetep7+vnyvuqqw#_z6_yf^g0jieek)0ody(h8w*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
